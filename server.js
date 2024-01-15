e44e40835d0b67d5ab2d6216b9cb5d099764ffab
M-Imai01 <fruitjuice877@gmail.com>
2024-01-15 16:03:33 +0900

    PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
-const PORT = 3000;
+// const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -18,6 +18,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(PORT, () => {
-  console.log("listening on 3030");
+server.listen(process.env.PORT || 3000, () => {
+  console.log("listening on 3000");
 });
